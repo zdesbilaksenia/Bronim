1687c354cc342f826454358a80e9323bf1574ac9
Artyom Shirshov <artyomsh01@yandex.ru>
2022-04-19 14:17:04 +0300

    Fixed mistakes

--- app/src/main/java/com/yo/bronim/managers/AuthorizationManager.kt
+++ app/src/main/java/com/yo/bronim/managers/AuthorizationManager.kt
@@ -4,10 +4,10 @@ import com.yo.bronim.models.AuthorizeCallback
 import com.yo.bronim.models.UserAuthorization
 import com.yo.bronim.providers.AuthorizationPageProvider
 
-class AuthorizationPageManager {
-    private val authorizationPageProvider = AuthorizationPageProvider()
+object AuthorizationManager {
+    private val authorizationProvider = AuthorizationPageProvider()
 
     fun authorize(callback: AuthorizeCallback, user: UserAuthorization) {
-        authorizationPageProvider.authorize(callback, user)
+        authorizationProvider.authorize(callback, user)
     }
 }
--- app/src/main/java/com/yo/bronim/viewmodels/AuthorizationPageViewModel.kt
+++ app/src/main/java/com/yo/bronim/viewmodels/AuthorizationPageViewModel.kt
@@ -1,12 +1,12 @@
 package com.yo.bronim.viewmodels
 
 import androidx.lifecycle.MutableLiveData
-import com.yo.bronim.managers.AuthorizationPageManager
+import com.yo.bronim.managers.AuthorizationManager
 import com.yo.bronim.models.UserAuthorization
 import com.yo.bronim.states.AuthorizationPageState
 
 class AuthorizationPageViewModel {
-    private val authorizationPageManager = AuthorizationPageManager()
+    private val authorizationPageManager = AuthorizationManager
     val authorizationPageState = MutableLiveData<AuthorizationPageState>()
 
     fun authorize(user: UserAuthorization) {
