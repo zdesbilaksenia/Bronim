43b7df41ed894c1180e693a4d540e983801e2ebf
zdesbilaksenia <ksju.nikitina@rambler.ru>
2022-05-08 18:59:56 +0300

    save

--- app/src/main/java/com/yo/bronim/MainActivity.kt
+++ app/src/main/java/com/yo/bronim/MainActivity.kt
@@ -1,19 +1,48 @@
 package com.yo.bronim
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mPermissionResult = registerForActivityResult(
+        RequestPermission()
+    ) { result ->
+        if (result) {
+            Log.e("TAG", "onActivityResult: PERMISSION GRANTED")
+        } else {
+            Toast.makeText(
+                this, "Не удалось получить данные о местоположении", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mPermissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+
+        MapKitFactory.setApiKey(ai.metaData["mapKey"].toString())
+        MapKitFactory.initialize(this)
 
         val navController =
             findViewById<FragmentContainerView>(R.id.nav_host_fragment_container)
--- app/src/main/java/com/yo/bronim/fragments/map/MapFragment.kt
+++ app/src/main/java/com/yo/bronim/fragments/map/MapFragment.kt
@@ -1,57 +1,169 @@
 package com.yo.bronim.fragments.map
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
+import android.util.Log
+import com.yandex.mapkit.logo.HorizontalAlignment.LEFT
+import com.yandex.mapkit.logo.VerticalAlignment.BOTTOM
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.ObjectEvent
+import com.yandex.mapkit.logo.Alignment
+import com.yandex.mapkit.map.*
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.image.ImageProvider
 import com.yo.bronim.R
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class MapFragment : Fragment(), UserLocationObjectListener, CameraListener {
+    private var mapView: MapView? = null
+    private var userLocationLayer: UserLocationLayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = view.findViewById(R.id.mapview)
+
+
+        checkPermission()
+        val mapLogoAlignment = Alignment(LEFT, BOTTOM)
+        mapView?.map?.logo?.setAlignment(mapLogoAlignment)
+        user_location_fab.setImageResource(R.drawable.ic_my_location_black_24dp)
+        mapView?.map?.move(
+            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0f),
+            null
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    private fun checkPermission() {
+        val permissionFineLocation = checkSelfPermission(activity as Context, ACCESS_FINE_LOCATION)
+        val permissionCoarseLocation =
+            checkSelfPermission(activity as Context, ACCESS_COARSE_LOCATION)
+        if (permissionFineLocation == PERMISSION_GRANTED || permissionCoarseLocation == PERMISSION_GRANTED) {
+            prepareMap()
+            Log.d("PERMISSION", "Granted ${permissionFineLocation}, ${permissionCoarseLocation}")
+        } else {
+            Toast.makeText(
+                activity, "Не удалось получить данные о местоположении", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun prepareMap() {
+        userLocationLayer = mapView?.mapWindow?.let {
+            MapKitFactory.getInstance().createUserLocationLayer(
+                it
+            )
+        }
+        userLocationLayer?.isVisible = true
+        userLocationLayer?.isHeadingEnabled = true
+        userLocationLayer?.setObjectListener(this)
+
+        mapView?.map?.addCameraListener(this)
+
+//        cameraUserPosition()
+
+//        permissionLocation = true
+    }
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = MapFragment()
+        const val requestPermissionLocation = 1
+    }
+
+    override fun onObjectAdded(userLocationView: UserLocationView) {
+        userLocationLayer!!.setAnchor(
+            PointF((mapView!!.width * 0.5).toFloat(), (mapView!!.height * 0.5).toFloat()),
+            PointF((mapView!!.width * 0.5).toFloat(), (mapView!!.height * 0.83).toFloat())
+        )
+
+//        userLocationView.arrow.setIcon(
+//            ImageProvider.fromResource(
+//                activity, R.drawable.user_arrow
+//            )
+//        )
+
+        val pinIcon = userLocationView.pin.useCompositeIcon()
+
+//        pinIcon.setIcon(
+//            "icon",
+//            ImageProvider.fromResource(activity, R.drawable.icon),
+//            IconStyle().setAnchor(PointF(0f, 0f))
+//                .setRotationType(RotationType.ROTATE)
+//                .setZIndex(0f)
+//                .setScale(1f)
+//        )
+//
+//        pinIcon.setIcon(
+//            "pin",
+//            ImageProvider.fromResource(activity, R.drawable.search_result),
+//            IconStyle().setAnchor(PointF(0.5f, 0.5f))
+//                .setRotationType(RotationType.ROTATE)
+//                .setZIndex(1f)
+//                .setScale(0.5f)
+//        )
+
+//        userLocationView.accuracyCircle.fillColor = Color.BLUE and -0x66000001
+//        userLocationView.pin.setIcon(fromResource(activity, R.drawable.ic_fav))
+//        userLocationView.arrow.setIcon(fromResource(activity, R.drawable.ic_fav))
+//        userLocationView.accuracyCircle.fillColor = BLUE
+    }
+
+    override fun onObjectRemoved(p0: UserLocationView) {
+    }
+
+    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
+    }
+
+    override fun onCameraPositionChanged(
+        p0: Map,
+        p1: CameraPosition,
+        p2: CameraUpdateReason,
+        finished: Boolean
+    ) {
+        userLocationLayer?.setAnchor(
+            PointF((mapView!!.width * 0.5).toFloat(), (mapView!!.height * 0.5).toFloat()),
+            PointF((mapView!!.width * 0.5).toFloat(), (mapView!!.height * 0.83).toFloat())
+        )
+
+//        user_location_fab.setImageResource(R.drawable.ic_my_location_black_24dp)
     }
 }
